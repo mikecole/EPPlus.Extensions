d9f49213a4ba2c88eecd6a5cc6605b1f338708ef
faredoon <faredoon@yahoo.com>
2016-11-14 20:47:01 +1100

    Added ToDataTable overload with various options
    Added ToDataTable overload with various options like a predicate for identifying the footer row, custom start and end column indices and custom column names. Used ToDataTable in ToDataSet method.

--- src/EPPlus.Extensions/EPPlusExtensions.cs
+++ src/EPPlus.Extensions/EPPlusExtensions.cs
@@ -1,7 +1,8 @@
-﻿using System.Data;
+using System.Data;
 using System.Linq;
 using OfficeOpenXml;
 using System;
+using System.Collections.Generic;
 
 namespace EPPlus.Extensions
 {
@@ -16,10 +17,16 @@ namespace EPPlus.Extensions
         /// <returns></returns>
         public static DataSet ToDataSet(this ExcelPackage package, bool firstRowContainsHeader = false)
         {
-            var headerRow = firstRowContainsHeader ? 1 : 0;
-            return ToDataSet(package, headerRow);
+            try
+            {
+                var headerRow = firstRowContainsHeader ? 1 : 0;
+                return ToDataSet(package, headerRow);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
-
 
         /// <summary>
         /// Extracts a DataSet from the ExcelPackage.
@@ -32,44 +39,32 @@ namespace EPPlus.Extensions
         {
             if (headerRow < 0)
             {
-                throw new ArgumentException("headerRow must be 0 or greater.");
+                throw new ArgumentOutOfRangeException(nameof(headerRow), headerRow, "Must be 0 or greater.");
             }
-            var result = new DataSet();
 
-
-            foreach (var sheet in package.Workbook.Worksheets)
+            try
             {
-                var table = ToDataTable(sheet, headerRow);
+                var result = new DataSet();
 
 
-                result.Tables.Add(table);
+                foreach (var sheet in package.Workbook.Worksheets)
+                {
+                    var table = ToDataTable(sheet, headerRow);
+
+
+                    result.Tables.Add(table);
+                }
+
+
+                return result;
             }
-
-
-            return result;
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        /// <summary>
-        /// Extracts a DataTable from the ExcelWorksheet object.
-        /// </summary>
-        /// <param name="package">The <see cref="ExcelWorksheet"/> object.</param>
-        /// <param name="sheet">if set to <c>true</c> [first row contains header].</param>
-        /// <returns></returns>
-        public static DataTable ToDataTable(this ExcelWorksheet sheet, bool firstRowContainsHeader = false)
-        {
-            var headerRow = firstRowContainsHeader ? 1 : 0;
-            return ToDataTable(sheet, headerRow);
-        }
-
-
-        /// <summary>
-        /// Extracts a DataTable from the ExcelWorksheet object.
-        /// </summary>
-        /// <param name="sheet">The Excel package.</param>
-        /// <param name="headerRow">The header row. Use 0 if there is no header row. Value must be 0 or greater.</param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException">headerRow must be 0 or greater.</exception>
-        public static DataTable ToDataTable(this ExcelWorksheet sheet, int headerRow = 0)
+        private static DataTable ToDataTable(this ExcelWorksheet sheet, int headerRow = 0, Func<ExcelRangeBase, bool> footerRowPredicate = null, int startColumn = 1, int endColumn = -1, List<string> columnNames = null)
         {
             if (headerRow < 0)
             {
@@ -83,27 +78,53 @@ namespace EPPlus.Extensions
             {
                 sheetStartRow = headerRow;
             }
-            var columns = from firstRowCell in sheet.Cells[sheetStartRow, 1, sheetStartRow, sheet.Dimension.End.Column]
-                          select new DataColumn(headerRow > 0 ? firstRowCell.Text : $"Column {firstRowCell.Start.Column}");
 
+            int maxColumnIndex = endColumn == -1 ? sheet.Dimension.End.Column : endColumn;
+
+            IEnumerable<DataColumn> columns = null;
+
+            //If user provides column names, we use them
+            if (columnNames == null)
+            {
+                columns = from firstRowCell in sheet.Cells[sheetStartRow, startColumn, sheetStartRow, maxColumnIndex]
+                          select new DataColumn(headerRow > 0 ? firstRowCell.Text : $"Column {firstRowCell.Start.Column}");
+            }
+            else
+            {
+                columns = columnNames.Select(columnName => new DataColumn(columnName));
+            }
 
             table.Columns.AddRange(columns.ToArray());
 
-
             var startRow = headerRow > 0 ? sheetStartRow + 1 : sheetStartRow;
-
 
             for (var rowIndex = startRow; rowIndex <= sheet.Dimension.End.Row; rowIndex++)
             {
-                var inputRow = sheet.Cells[rowIndex, 1, rowIndex, sheet.Dimension.End.Column];
+
+                var inputRow = sheet.Cells[rowIndex, startColumn, rowIndex, maxColumnIndex];
+
+                //if user has provided a predicate that determines if a row is a 'footer'
+                if (footerRowPredicate != null)
+                {
+                    //If any cell in the current row satisifies the predicate, we bail out and consider that we have reached the footer row.
+                    if (inputRow.Any(cell => footerRowPredicate(cell)))
+                    {
+                        return table;
+                    }
+                }
+
                 var row = table.Rows.Add();
+                int colIndex = 0;
+
                 foreach (var cell in inputRow)
                 {
-                    row[cell.Start.Column - 1] = cell.Text;
+                    row[colIndex] = cell.Text;
+                    colIndex++;
                 }
             }
 
             return table;
         }
+
     }
 }
